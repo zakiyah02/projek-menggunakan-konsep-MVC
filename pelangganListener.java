efd66e6008700a21d749cf5926cb43b0365a0d6b
zakiyah02 <93088497+zakiyah02@users.noreply.github.com>
2022-03-13 20:07:35 -0700

    Add files via upload

---
+++ pelangganListener.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.zakiyah.latihanmvc.event;
+
+import edu.zakiyah.latihanmvc.model.pelangganmodel;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface pelangganListener {
+    
+    public void onChange(pelangganmodel pelanggan);
+
+       
+}
